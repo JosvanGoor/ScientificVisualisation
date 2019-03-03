2b56c0e484cd881bdec49b408ce0dcf5b2aab1d2
Jos van Goor <josvangoor@gmail.com>
2019-03-03 16:10:07 +0100

    removed unneeded shader code

--- shaders/glyph2d_vertex.glsl
+++ shaders/glyph2d_vertex.glsl
@@ -8,19 +8,6 @@ uniform mat4 projection;
 
 const float PI = 3.1415926535897932384626433832795;
 
-mat4 rotation_mat(vec3 axis, float angle)
-{
-    axis = normalize(axis);
-    float s = sin(angle);
-    float c = cos(angle);
-    float oc = 1.0 - c;
-    
-    return mat4(oc * axis.x * axis.x + c,           oc * axis.x * axis.y - axis.z * s,  oc * axis.z * axis.x + axis.y * s,  0.0,
-                oc * axis.x * axis.y + axis.z * s,  oc * axis.y * axis.y + c,           oc * axis.y * axis.z - axis.x * s,  0.0,
-                oc * axis.z * axis.x - axis.y * s,  oc * axis.y * axis.z + axis.x * s,  oc * axis.z * axis.z + c,           0.0,
-                0.0,                                0.0,                                0.0,                                1.0);
-}
-
 void main()
 {
     vec2 pos;
