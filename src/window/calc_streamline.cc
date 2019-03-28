126b7450fa6e8cb737b840745cc3583085693d6c
voldelord <r.f.niel@student.rug.nl>
2019-03-28 16:20:52 +0100

    small fix?

--- src/window/calc_streamline.cc
+++ src/window/calc_streamline.cc
@@ -14,8 +14,8 @@ void Window::calc_streamline(size_t x, size_t y, size_t z)
         stream_lines.push_back(curY * square);
         stream_lines.push_back(z * square);
 
-        newX = curX - d_simulation.gridsize() * (1 + d_simulation.timestep() * d_simulation.history_x()[idx][curX + curY * d_simulation.gridsize()]);
-        newY = curY - d_simulation.gridsize() * (1 + d_simulation.timestep() * d_simulation.history_y()[idx][curX + curY * d_simulation.gridsize()]);
+        newX = curX + d_simulation.gridsize() - d_simulation.gridsize() * (d_simulation.timestep() * d_simulation.history_x()[idx][curX + curY * d_simulation.gridsize()]);
+        newY = curY + d_simulation.gridsize() - d_simulation.gridsize() * (d_simulation.timestep() * d_simulation.history_y()[idx][curX + curY * d_simulation.gridsize()]);
 
         curX = newX % d_simulation.gridsize();
         curY = newY % d_simulation.gridsize();
