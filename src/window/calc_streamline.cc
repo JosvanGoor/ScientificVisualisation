bc08303912e464334baf73ad7134ea320de2c8ab
voldelord <r.f.niel@student.rug.nl>
2019-04-02 16:56:09 +0200

    streamtube now uses bilinear interpolation

--- src/window/calc_streamline.cc
+++ src/window/calc_streamline.cc
@@ -1,5 +1,16 @@
 #include "window.ih"
 
+/*  Bilinear interpolation where p00 is scalar value at top left of unit square,
+    p01 top right, p10 bottom left and p11 bottom right*/
+double interpolate(double x, double y, double p00, double p01, double p10, double p11)
+{
+    if (x > 1 || x < 0 || y > 1 || y < 0)
+        throw "x and y should be in range [0,1]";
+
+    return p00 * (1 - x) * (1 - y) + p10 * x * (1 - y) + p01 * (1 - x) * y + p11 * x * y;
+}
+
+
 void Window::calc_streamline(size_t x, size_t y, size_t z)
 {
     double curX = x;
@@ -13,8 +24,17 @@ void Window::calc_streamline(size_t x, size_t y, size_t z)
         stream_lines.push_back(curY * square);
         stream_lines.push_back(idx * square);
 
-        double posX = curX + d_simulation.gridsize() * (d_simulation.timestep() * d_simulation.history_x()[idx][int(curX + curY * d_simulation.gridsize() + 0.5)]);
-        double posY = curY + d_simulation.gridsize() * (d_simulation.timestep() * d_simulation.history_y()[idx][int(curX + curY * d_simulation.gridsize() + 0.5)]);
+        double velX00 = d_simulation.history_x()[idx][int(curX) + int(curY) * d_simulation.gridsize()];
+        double velX01 = d_simulation.history_x()[idx][int(curX) + 1 + int(curY) * d_simulation.gridsize()];
+        double velX10 = d_simulation.history_x()[idx][int(curX) + (int(curY) + 1) * d_simulation.gridsize()];
+        double velX11 = d_simulation.history_x()[idx][int(curX) + 1 + (int(curY) + 1) * d_simulation.gridsize()];
+        double velY00 = d_simulation.history_y()[idx][int(curX) + int(curY) * d_simulation.gridsize()];
+        double velY01 = d_simulation.history_y()[idx][int(curX) + 1 + int(curY) * d_simulation.gridsize()];
+        double velY10 = d_simulation.history_y()[idx][int(curX) + (int(curY) + 1) * d_simulation.gridsize()];
+        double velY11 = d_simulation.history_y()[idx][int(curX) + 1 + (int(curY) + 1) * d_simulation.gridsize()];
+
+        double posX = curX + d_simulation.gridsize() * (d_simulation.timestep() * interpolate(curX - int(curX), curY - int(curY), velX00, velX01, velX10, velX11));
+        double posY = curY + d_simulation.gridsize() * (d_simulation.timestep() * interpolate(curX - int(curX), curY - int(curY), velY00, velY01, velY10, velY11));
 
         curX = posX >= 0.0 ? posX : -(1 - posX);
         curX += d_simulation.gridsize();
